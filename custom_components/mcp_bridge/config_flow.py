c55818df9544237fc311945720f02ceff5d69de0
luolin046 <luolin046@ke.com>
2025-05-22 15:20:42 +0800

    optimiz

--- custom_components/mcp_bridge/config_flow.py
+++ custom_components/mcp_bridge/config_flow.py
@@ -8,7 +8,7 @@ class MCPConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         if user_input is not None:
-            return self.async_create_entry(title="HA MCP xiaozhi Bridge", data=user_input)
+            return self.async_create_entry(title="MCP XiaoZhi Bridge", data=user_input)
 
         return self.async_show_form(
             step_id="user",
